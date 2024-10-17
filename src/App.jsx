cec221b453c974b98749b670c7d15161f177ef8f
rahul-17537 <rahul-17537@zohocorp.com>
2024-10-17 13:58:49 +0530

    react sample

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+import React from "react"
+
+class App extends React.Component {
+  render(){
+    return (
+      <div className="card">
+         <div className="texts">
+           <h1>Flower</h1>
+           <p>
+           A flower, also known as a bloom or blossom, is the reproductive structure found in flowering plants (plants of the division Angiospermae). 
+           Flowers consist of a combination of vegetative organs – sepals that enclose and protect the developing flower. These petals attract pollinators, and reproductive organs that produce gametophytes, which in flowering plants produce gametes. 
+           The male gametophytes, which produce sperm, are enclosed within pollen grains produced in the anthers. 
+           The female gametophytes are contained within the ovules produced in the ovary.
+ 
+           Most flowering plants depend on animals, such as bees, moths, and butterflies, to transfer their pollen between different flowers, and have evolved to attract these pollinators by various strategies, including brightly colored, conspicuous petals, attractive scents, and the production of nectar, a food source for pollinators. 
+           In this way, many flowering plants have co-evolved with pollinators to be mutually dependent on services they provide to one another—in the plant's case, a means of reproduction; in the pollinator's case, a source of food.
+  </p>
+         </div>
+         <img src="image/flowerss2.jpg" alt="An Error Occured!"/>
+       </div>
+   );
+  }
+}
+
+export default App;
